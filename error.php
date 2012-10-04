27f0d53f812c96ac4750560d5f03c39e6d6804b6
carcho <carnicxld@hotmail.com>
2012-10-03 21:15:30 -0300

    Solo estoy probando!!
    Haber ahora

--- error.php
+++
@@ -1,8 +0,0 @@
-<html>
-  <head>
-    <title>Todo Liso</title>
-  </head>
-  <body>
-    <h1>Error.</h1>
-  </body>
-</html>
--- insession.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head>
-    <title>Dentro de la sesion</title>
-  </head>
-  <body>
-    <h1>Estas dentro de la sesion</h1>
-    <a href="controlador.php?salir=salir123">Salir</a>
-  </body>
-</html>
